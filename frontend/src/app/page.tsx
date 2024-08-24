cf5d4b43a723eb2cae9faa33117595764487f1ac
PrivateEye_Zero <silverbulletrye4869@gmail.com>
2024-08-24 15:13:11 +0900

    refac: loginのurl変更

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,17 @@
+"use client";
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { MyProvider } from '@/contexts/MyContext';
-import Login from '@/pages/login';
-import Debug from '@/pages/debug';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { MyProvider } from "@/contexts/MyContext";
+import Login from "@/pages/user/login";
+import Debug from "@/pages/debug";
 
 function App() {
   return (
     <MyProvider>
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/user/login" element={<Login />} />
           <Route path="/debug" element={<Debug />} />
         </Routes>
       </Router>
--- frontend/src/pages/user/login.tsx
+++ frontend/src/pages/user/login.tsx
@@ -1,18 +1,17 @@
-"use client"
+"use client";
 
 import { BACKEND_URL } from "@/basic_info";
 import { useMyContext } from "@/contexts/MyContext";
 import { Box, Button, Input, VStack } from "@chakra-ui/react";
 import { useState } from "react";
-import { useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 
-const url = BACKEND_URL+"/auth"; // Replace with your target URL
+const url = BACKEND_URL + "/auth/login"; // Replace with your target URL
 export default function Login() {
   const [id, setId] = useState("");
   const [pass, setPass] = useState("");
-  const {sessionId, setSessionId} = useMyContext();
+  const { sessionId, setSessionId } = useMyContext();
   const router = useRouter();
-
 
   const handleSubmit = async () => {
     try {
@@ -21,12 +20,12 @@ export default function Login() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({id,pass}),
+        body: JSON.stringify({ id, pass }),
       });
       console.log(response);
       const data = await response.json();
       console.log(data); // Handle the response data as needed
-      if(data.result === "success"){
+      if (data.result === "success") {
         setSessionId(data.session_id);
         router.push("/debug");
       }
@@ -38,25 +37,23 @@ export default function Login() {
   return (
     <Box p={4}>
       <VStack spacing={4}>
+        <Box>Session: {sessionId}</Box>
         <Box>
-          Session: {sessionId}
+          ID:
+          <Input
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            color={"black"}
+          />
         </Box>
         <Box>
-        ID: 
-        <Input
-          value={id}
-          onChange={(e) => setId(e.target.value)}
-          color={"black"}
-        />
-        </Box>
-        <Box>
-        PASSWORD: 
-        <Input
-          type="password"
-          value={pass}
-          onChange={(e) => setPass(e.target.value)}
-          color={"black"}
-        />
+          PASSWORD:
+          <Input
+            type="password"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            color={"black"}
+          />
         </Box>
         <Button onClick={handleSubmit}>Submit</Button>
       </VStack>
